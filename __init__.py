545e123bd3a1437e608a6af826caeaab562eb577
Bartek878 <57801244+Bartek878@users.noreply.github.com>
2020-03-09 20:42:58 +0100

    Update class variables

--- __init__.py
+++ __init__.py
@@ -1,20 +1,20 @@
 #Selenium based app for weather forecast.
 
 from weather_import import WeatherCreator
-get_weather = WeatherCreator()
+weatherCreator = WeatherCreator()
 from send_email import MailSender
-send_email = MailSender()
+mailSender = MailSender()
 from visualisation import ChartCreator
-create_visualisation = ChartCreator()
+chartCreator = ChartCreator()
 
 def main():
 
-    getting_weather.open_webpage()
-    getting_weather.choose_city()
-    get_weather.get_current_weather()
-    get_weather.write_to_csv()
-    send_email.send_file()
-    create_visualisation.get_data_for_chart()
-    create_visualisation.create_chart()
+    weatherCreator.open_webpage()
+    weatherCreator.choose_city()
+    weatherCreator.get_current_weather()
+    weatherCreator.write_to_csv()
+    mailSender.send_file()
+    chartCreator.get_data_for_chart()
+    chartCreator.create_chart()
 
 main()
